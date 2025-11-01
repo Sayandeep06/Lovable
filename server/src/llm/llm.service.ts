4c58ea723aebd3329d6cef04633393c51d45d31c
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-11-02 00:23:29 +0530

    onmodule init

--- server/src/llm/llm.service.ts
+++ server/src/llm/llm.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { GoogleGenAI } from "@google/genai";
 
 @Injectable()
-export class LlmService {
+export class LlmService implements OnModuleInit {
     private ai: GoogleGenAI;
+
     constructor(){
-        const ai = new GoogleGenAI({});
+        this.ai = new GoogleGenAI({});
     }
+
+    async onModuleInit() {
+        await this.generateContent();
+    }
+
     async generateContent() {
         const response = await this.ai.models.generateContent({
-        model: 'gemini-2.5-flash',
-        contents: "Explain how AI works in a few words",
+            model: 'gemini-2.5-flash',
+            contents: "Explain how AI works in a few words",
         });
         console.log(response.text);
         return response.text;
